cf24a4fe1143e7cab595cf05f41dafe8d0bd5470
dengtao <981376577@qq.com>
2024-10-09 16:43:20 +0800

    xx

---
+++ taotao-cloud-member-infrastructure/src/main/java/com/taotao/cloud/member/infrastructure/repository/DictDomainRepositoryImpl.java
@@ -0,0 +1,11 @@
+package com.taotao.cloud.member.infrastructure.repository;
+
+import com.taotao.cloud.member.domain.dict.repository.DictDomainRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class DictDomainRepositoryImpl implements DictDomainRepository {
+
+}
