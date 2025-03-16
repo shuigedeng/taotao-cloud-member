8f9a46e6bae11799e3596a2fec59e1d98ec8cf61
shuigedeng <981376577@qq.com>
2025-03-16 08:47:08 +0800

    build: 升级2025.04

---
+++ taotao-cloud-member-facade/src/main/java/com/taotao/cloud/member/facade/sys/SysAclServiceImpl.java
@@ -0,0 +1,7 @@
+package com.taotao.cloud.member.facade.sys;
+
+
+import com.taotao.cloud.member.application.acl.SysAclService;
+
+public class SysAclServiceImpl implements SysAclService {
+}
--- taotao-cloud-member-integration/src/main/java/com/taotao/cloud/member/integration/sys/SysAclServiceImpl.java
+++
@@ -1,7 +0,0 @@
-package com.taotao.cloud.member.integration.sys;
-
-
-import com.taotao.cloud.member.application.acl.SysAclService;
-
-public class SysAclServiceImpl implements SysAclService {
-}
