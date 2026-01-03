ac5d76b2970338ae223bf268eb7f954bde747853
shuigedeng <981376577@qq.com>
2026-01-03 18:14:22 +0800

    开启2026.02

--- taotao-cloud-member-assembly/src/test/java/com/taotao/cloud/member/biz/SpringTest.java
+++ taotao-cloud-member-assembly/src/test/java/com/taotao/cloud/member/biz/SpringTest.java
@@ -39,7 +39,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
  * SpringTest
  *
  * @author shuigedeng
- * @version 2026.01
+ * @version 2026.02
  * @since 2025-12-19 09:30:45
  */
 @SpringBootTest
--- taotao-cloud-member-common/src/main/java/com/taotao/cloud/member/common/data/dataobj/MemberDO.java
+++ taotao-cloud-member-common/src/main/java/com/taotao/cloud/member/common/data/dataobj/MemberDO.java
@@ -20,7 +20,7 @@ package com.taotao.cloud.member.common.data.dataobj;
  * MemberDO
  *
  * @author shuigedeng
- * @version 2026.01
+ * @version 2026.02
  * @since 2025-12-19 09:30:45
  */
 public class MemberDO {
